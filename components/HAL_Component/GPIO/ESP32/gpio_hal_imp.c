613dcb7fd9a98f2b004ff7163cccd97922535f41
Mohammed Hisham <mohammed.hisham171297@gmail.com>
2023-03-25 12:41:59 +0200

    Implement ISR handling for ESP32 GPIO

--- components/HAL_Component/GPIO/ESP32/gpio_hal_imp.c
+++ components/HAL_Component/GPIO/ESP32/gpio_hal_imp.c
@@ -7,6 +7,8 @@
 
 error_t gpio_hal_init(void)
 {
+    gpio_install_isr_service(0);
+
     return OK;
 }
 
@@ -28,12 +30,33 @@ error_t gpio_hal_config(gpio_hal_pin_t pin_num, const gpio_hal_config_t* p_confi
     {
         case GPIO_HAL_MODE_INPUT:
             l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_INPUT));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_DISABLE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_disable(pin_num));
             break;
         case GPIO_HAL_MODE_OUTPUT_PP:
             l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_OUTPUT));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_DISABLE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_disable(pin_num));
             break;
         case GPIO_HAL_MODE_OUTPUT_OD:
             l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_OUTPUT_OD));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_DISABLE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_disable(pin_num));
+            break;
+        case GPIO_HAL_MODE_IT_RISING:
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_INPUT));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_POSEDGE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_enable(pin_num));
+            break;
+        case GPIO_HAL_MODE_IT_FALLING:
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_INPUT));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_NEGEDGE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_enable(pin_num));
+            break;
+        case GPIO_HAL_MODE_IT_RISING_FALLING:
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_direction(pin_num, GPIO_MODE_INPUT));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_set_intr_type(pin_num, GPIO_INTR_ANYEDGE));
+            l_ret &= GPIO_HAL_RET_CHECK(gpio_intr_enable(pin_num));
             break;
     }
 
@@ -72,4 +95,18 @@ gpio_hal_level_t gpio_hal_read(gpio_hal_pin_t pin_num)
 {
     return (gpio_hal_level_t) gpio_get_level(pin_num) ? 
                                 GPIO_HAL_LEVEL_HIGH : GPIO_HAL_LEVEL_LOW;
+}
+
+error_t gpio_hal_register_callback(gpio_hal_pin_t pin_num, gpio_hal_callback_t cb)
+{
+    BOOL l_ret;
+
+    if(!GPIO_HAL_IS_PIN(pin_num) || !cb)
+    {
+        return FAILED;
+    }
+
+    l_ret = GPIO_HAL_RET_CHECK(gpio_isr_handler_add(pin_num, (void*) cb, (void*) NULL));
+
+    return (l_ret ? OK : FAILED);
 }
--- main/main.c
+++ main/main.c
@@ -13,7 +13,7 @@
 
 #include "gpio_hal_itf.h"
 
-#define LED_PIN     PIN_1
+#define LED_PIN     PIN_3
 #define BUTTON_BIN  PIN_0 
 
 gpio_hal_level_t led_state;
@@ -43,6 +43,8 @@ void app_main()
     gpio_hal_config(LED_PIN, &led_config);
     gpio_hal_config(BUTTON_BIN, &button_config);
     
+    gpio_hal_write(LED_PIN, GPIO_HAL_LEVEL_HIGH);
+
     while (1)
     {
         vTaskDelay(500 / portTICK_PERIOD_MS);
