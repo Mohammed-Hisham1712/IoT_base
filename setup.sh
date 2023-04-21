3dd6da7a3b988e1ccde613370458c8490e0c1513
Mohammed Hisham <mohammed.hisham171297@gmail.com>
2023-04-22 01:13:42 +0200

    Modifying setup.sh to run make menuconfig for convenience
    To open menconfig run ./setup.sh product_config/ESP*.config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# This script is used to run instead of make CONFIG_PATH=product_config/*.config menuconfig
+# Usage: ./setup.sh <config_file>   -> e.g., ./setup.sh product_config/ESP8266_GPIO.config
+
 name=$(basename ${BASH_SOURCE:-$0})
 path=$(dirname $(realpath ${name}))
 
 export COMPONENTS_PATH_ENV=$path/components
+
+make CONFIG_PATH=$1 menuconfig
